c9d2ca0a1752a548ff8a371590dadd0d1d938993
Tiha <tgtihamer@yahoo.com>
2023-01-07 13:24:51 +0200

    implement Slice operation

---
+++ src/Slice.java
@@ -0,0 +1,8 @@
+public class Slice {
+    public static Polynomial slice(Polynomial p, int highest, int lowest) {
+        return new Polynomial(
+                highest - lowest,
+                rank -> p.get(rank + lowest)
+        );
+    }
+}
